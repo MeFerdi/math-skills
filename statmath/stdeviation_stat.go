8f7dd1886808575af07fb26379da5b47efee680d
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-06-10 19:44:08 +0300

    Fix: Optimized variance and standard deviation functions efficiently

--- statmath/stdeviation_stat.go
+++ statmath/stdeviation_stat.go
@@ -6,27 +6,6 @@ import (
 
 // StandardDeviation calculates the standard deviation of a given slice of float64 values.
 func StandardDeviation(data []float64) float64 {
-	// Get the length of the data slice.
-	n := float64(len(data))
-	// If the length of the data slice is 0, return 0.
-	if n == 0 {
-		return 0
-	}
-
-	// Calculate the mean of the data.
-	var mean float64
-	for _, value := range data {
-		mean += value
-	}
-	mean /= n
-
-	// Calculate the variance of the data.
-	var variance float64
-	for _, value := range data {
-		variance += (value - mean) * (value - mean)
-	}
-	variance /= n
-
 	// Calculate the square root of the variance to get the standard deviation.
-	return math.Sqrt(variance)
+	return math.Sqrt(Variance(data))
 }
--- statmath/variance_stat.go
+++ statmath/variance_stat.go
@@ -6,26 +6,17 @@ import (
 
 // Variance calculates the variance of a given slice of float64 values.
 func Variance(numbers []float64) float64 {
-	// Initialize a variable to store the sum of all numbers.
-	sum := 0.0
-
-	for _, num := range numbers {
-		sum += num
-	}
-
-	// Calculate the mean by dividing the sum by the number of values.
-	mean := float64(sum) / float64(len(numbers))
+	// Calculate the mean.
+	mean := Average(numbers)
 
 	// Initialize a variable to store the sum of squared differences from the mean.
-	sum = 0
+	variance := 0.0
 
 	// Calculate the sum of squared differences from the mean.
 	for _, num := range numbers {
-
 		diff := float64(num - mean)
-
-		sum += math.Pow(diff, 2)
+		variance += math.Pow(diff, 2)
 	}
 
-	return float64(sum) / float64(len(numbers))
+	return float64(variance) / float64(len(numbers))
 }
